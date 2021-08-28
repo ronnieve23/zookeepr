d612f0596b8dd645f4f3b120f071b399818ca302
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-08-28 13:50:56 -0400

    added html

--- server.js
+++ server.js
@@ -6,9 +6,8 @@ const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// parse incoming string or array data
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
 app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
@@ -90,13 +89,16 @@ app.post('/api/animals', (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = animals.length.toString();
 
-  // if any data in req.body is incorrect, send 400 error back
   if (!validateAnimal(req.body)) {
     res.status(400).send('The animal is not properly formatted.');
   } else {
     const animal = createNewAnimal(req.body, animals);
     res.json(animal);
   }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
